df29054348fef7b6d5da94cb64822803c0346866
Dana <Dana@Kens-MacBook-Pro.local>
2019-04-05 18:17:33 -0400

    Added another button

--- myJSfile.js
+++ myJSfile.js
@@ -3,3 +3,11 @@ document.getElementById("demo").innerHTML = "This is my new text";
 }
 
 document.getElementById('demo').style.color = 'blue'
+
+function usingVariables(){
+var userName = prompt("What is your name?")
+	var textElement = document.getElementById("text")
+	textElement.innerHTML = "Hi " + userName
+	var header = document.getElementById("head")
+	header.innerHTML = header.innerHTML + "By" + userName
+}
