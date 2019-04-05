cb1ccdb54e75c0562029b00f34663737024ac294
Dana <Dana@Kens-MacBook-Pro.local>
2019-04-05 18:21:48 -0400

    Added another button

--- myJSfile.js
+++ myJSfile.js
@@ -9,5 +9,5 @@ var userName = prompt("What is your name?")
 	var textElement = document.getElementById("text")
 	textElement.innerHTML = "Hi " + userName
 	var header = document.getElementById("head")
-	header.innerHTML = header.innerHTML + "By" + userName
+	header.innerHTML = header.innerHTML + " By " + userName
 }
